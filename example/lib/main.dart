3c474634603a051e11030982f7d22198b86a2ddc
Soumajit Das <151773081+AT0423-Soumajit@users.noreply.github.com>
2024-06-10 22:26:17 +0530

    fixed gradle version

---
+++ example/lib/main.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
+import 'package:flutter_html_to_pdf_example/pdf_view.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late String generatedPdfFilePath;
+
+  @override
+  void initState() {
+    super.initState();
+    generateExampleDocument();
+  }
+
+  Future<void> generateExampleDocument() async {
+    final htmlContent = """
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <style>
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        th, td, p {
+          padding: 5px;
+          text-align: left;
+        }
+        </style>
+      </head>
+      <body>
+        <h2>PDF Generated with flutter_html_to_pdf plugin</h2>
+        
+        <table style="width:100%">
+          <caption>Sample HTML Table</caption>
+          <tr>
+            <th>Month</th>
+            <th>Savings</th>
+          </tr>
+          <tr>
+            <td>January</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>February</td>
+            <td>50</td>
+          </tr>
+        </table>
+        
+        <p>Image loaded from web</p>
+        <img src="https://i.imgur.com/wxaJsXF.png" alt="web-img">
+      </body>
+    </html>
+    """;
+
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    final targetPath = appDocDir.path;
+    final targetFileName = "example-pdf";
+
+    final generatedPdfFile = await FlutterHtmlToPdf.convertFromHtmlContent(
+        htmlContent, targetPath, targetFileName);
+    generatedPdfFilePath = generatedPdfFile.path;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Open Generated PDF Preview"),
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PDFScreen(
+                    path: generatedPdfFilePath,
+                  ),
+                ));
+          },
+        ),
+      ),
+    ));
+  }
+}
